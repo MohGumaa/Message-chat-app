ec2beef7d4889b42a4dca5e5d64ac05be0d70fc6
MohGumaa <gomaam14@gmail.com>
2020-05-18 08:01:25 +0530

    create web socketi for notify channel creation

--- application.py
+++ application.py
@@ -1,13 +1,24 @@
 import os
 
 from flask import Flask, render_template
-# from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-# socketio = SocketIO(app)
+#os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] ="secert!"
+socketio = SocketIO(app)
 
+channels = []
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", channels=channels)
+
+@socketio.on("new_channel")
+def new_channel(channelName):
+    """Check if the channel if exist return error else add to list."""
+    if channelName in channels:
+        emit("channel list", {"success": False, "error":"Channel already exist!"})
+    else:
+        channels.append(channelName)
+        emit("channel list", {"success": True, "channels":channels}, broadcast=True)
