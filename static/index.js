bab1a41ff9d4e3487474d69cb0a95a266df89005
MohGumaa <gomaam14@gmail.com>
2020-05-18 07:58:55 +0530

    Add feature of creating new channel

--- static/index.js
+++ static/index.js
@@ -1,7 +1,52 @@
-// Select DOM
-const name = document.querySelector("#name");
-
 document.addEventListener('DOMContentLoaded', () => {
+  // Check name of customer
+  showName();
+
+  // Connect to websocket
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+  // When connected, Listen to form creation submit
+  socket.on('connect', () => {
+    // Creating New Channel
+    document.querySelector("#channelCreation").onsubmit = () => {
+      // Emit the channel creation event using the input from User
+      const channelName = document.querySelector("#new-channel").value;
+      socket.emit("new_channel", channelName);
+      console.log(channelName)
+      // Prevent Form Submission
+      return false;
+    };
+  });
+
+  socket.on("channel list", data => {
+    if(data.success){
+      let output= '';
+      data.channels.forEach(channel => {
+        output += `<button>${channel}</button>`
+      });
+      document.querySelector("#left .drop-contents").innerHTML = output;
+    } else {
+      document.querySelector("#left b.text-danger").innerHTML = data.error;
+      document.querySelector("#left b.text-danger").style.display = "block";
+      console.log(data.error)
+      setTimeout(() => {
+        document.querySelector("#left b.text-danger").style.display = "none";
+      },3000);
+    }
+  });
+
+
+  // Add event listener  to button click on small screen
+  document.querySelector(".menu-btn").addEventListener('click', () => {
+    // Add and remove class
+    document.querySelector("#left").classList.toggle('show');
+  });
+});
+
+// Function name
+function showName() {
+  // Select DOM
+  const name = document.querySelector("#name");
 
   // When page is loaded check username if exist
   if (!localStorage.getItem("username")) {
@@ -23,17 +68,12 @@ document.addEventListener('DOMContentLoaded', () => {
    // listen when user submit Name
    document.querySelector("#userRegister").onsubmit = () => {
      localStorage.setItem("username", name.value);
-     console.log(localStorage.getItem('username'))
+     document.querySelector("#modal").style.opacity = 0;
+     document.querySelector("#modal").style.display = "none";
+     document.querySelector("#left p.mt-2 strong").innerHTML = localStorage.getItem("username");
      return false;
    }
-
   } else {
     document.querySelector("#left p.mt-2 strong").innerHTML = localStorage.getItem("username");
   }
-
-  // Add event listener  to button click on small screen
-  document.querySelector(".menu-btn").addEventListener('click', () => {
-    // Add and remove class
-    document.querySelector("#left").classList.toggle('show');
-  });
-});
+}
